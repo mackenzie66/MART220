2e57aa4fbc703c01f0a2b9bde3db3504b0375930
MacKenzie Dean <77417131+mackenzie66@users.noreply.github.com>
2024-01-29 22:25:01 -0700

    Add files via upload

---
+++ week 2/sketch.js
@@ -0,0 +1,65 @@
+
+
+function setup() {
+    createCanvas(400,400);
+  }
+  
+  function draw() {
+    background(220);
+//straw
+fill(190,230,200);
+rect(290,10,20,100)
+// glass
+fill(235,190, 220);
+rect(290,100,100,200);
+
+//whipcream
+fill(220,220, 220);
+triangle(400, 100, 335, 40, 275, 100);
+
+
+
+
+    //sauce
+    fill(255,65,0);
+    ellipse(200,300,350,200);
+    //cheese
+    fill(255,165,0);
+    ellipse(200,300,330,180);
+
+
+// left circle
+    fill(255,60,0);
+    ellipse(80,300,40,40);
+
+//right circle
+    ellipse(320,300,40,40);
+//top right circle
+    ellipse(270,250,40,40);
+//top left circle
+    ellipse(130,250,40,40);
+//bottom left circle
+    ellipse(270,350,40,40);
+//bottom right circle
+    ellipse(130,350,40,40);
+  //center circle
+    ellipse(200,300,40,40);
+
+    //small left circle
+    ellipse(140,300,35,35);
+   
+    //small right circle
+    ellipse(260,300,35,35);
+
+    //small top circle
+    ellipse(200,240,35,35);
+
+    //small top circle
+    ellipse(200,360,35,35);
+
+
+    
+  }
+
+ 
+ 
